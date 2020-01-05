743e8772de5d0ee937bd54eed3429ebd2bea87bf
Duong Vu <hdvu98@users.noreply.github.com>
2020-01-05 17:13:06 +0700

    update

--- CellPhone.cs
+++ CellPhone.cs
@@ -22,7 +22,7 @@ namespace DemoDAMFramework
         public Maker Maker { get; set; }
         
         [OneToOne("2","Price")]
-        [ForeignKey("2","ID","CellPhoneID")]
+        [ForeignKey("2", "ID", "CellPhoneID")]
         public Phone_Price Price { get; set; }
 
         [OneToMany("3","CellPhone_Color")]
